dc044359cc15bf9e4a99a4da67c67b3e44b4f500
Karamveer Singh <100985912+veerrai@users.noreply.github.com>
2025-03-28 13:01:34 +0530

    files add

--- components/OceanicJourneys.js
+++ components/OceanicJourneys.js
@@ -1,33 +1,67 @@
 "use client";
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 const OceanicJourneys = () => {
   return (
     <section className="oceanic-journeys container-fluid">
       <Container>
         <Row className="align-items-center">
-          {/* Text Section */}
           <Col md={6}>
-            <h2 className="text-primary fw-bold">Oceanic Journeys</h2>
+            <h2 className="text-primary fw-bold">About Trending Cruise </h2>
             <p>
-              Sail to exotic destinations on unforgettable cruise packages. Explore remote
-              islands, dive into pristine waters, and unwind on sandy shores. Discover the magic 
-              of coastal living, immerse yourself in local cultures, and let each port paint a picture 
-              of pure adventure and relaxation.
+              At Trending Cruise, we believe that a dream vacation should be
+              within reach for everyone. That’s why we offer unlimited deals and
+              discounts on cruise trips so you can plan your dream vacation at
+              minimal prices. Choose your desired cruise line, nd get ready to
+              immerse yourself in pure relaxation at each port.
             </p>
             <ul className="list-unstyled">
-              <li><i className="fas fa-check text-success"></i> 24/7 Customer Support</li>
-              <li><i className="fas fa-check text-success"></i> Value packed Offers</li>
-              <li><i className="fas fa-check text-success"></i> Gourmet Dining</li>
-              <li><i className="fas fa-check text-success"></i> Unlimited Destination</li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className="mx-1"
+                  style={{ color: "#13d313" }}
+                />
+                Exclusive Deals & Offers
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className="mx-1"
+                  style={{ color: "#13d313" }}
+                />
+                Personalized Cruise Packages
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className="mx-1"
+                  style={{ color: "#13d313" }}
+                />
+                Diverse Dining Options
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className="mx-1"
+                  style={{ color: "#13d313" }}
+                />
+                24*7 Customer Support
+              </li>
             </ul>
           </Col>
 
           {/* Image Section */}
           <Col md={6} className="text-center">
-            <img src="/images/ocean.png" alt="Cruise" className="cruise-image"  style={{ width: '50%'}} />
+            <img
+              src="/images/ocean.png"
+              alt="Cruise"
+              className="cruise-image"
+              style={{ width: "50%" }}
+            />
           </Col>
         </Row>
       </Container>
